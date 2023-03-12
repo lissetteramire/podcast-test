fb3dfae5f4ec5747d2d2e7e687cc63d22113c5af
Lissette Ramirez <lissette.ramirez.barillas@gmail.com~>
2023-03-12 05:09:13 -0600

    3.Test y componente contador y filtro  de vista principal

--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -2,6 +2,13 @@ const Principal = () => {
   return (
     <div>
       <h1>Podcaster</h1>
+      <hr />
+      <div className="contenedor-filtro">
+        <label htmlFor="" className="contador">
+          100
+        </label>
+        <input type="text" className="filtro" placeholder="Filter podcasts..." />
+      </div>
     </div>
   )
 }
--- src/Components/Principal.test.js
+++ src/Components/Principal.test.js
@@ -6,4 +6,12 @@ describe("Tests de vista Principal", () => {
     render(<Principal />)
     expect(screen.getByText("Podcaster")).toBeInTheDocument()
   })
+  test("Debe mostrar una etiqueta de filtro con los 100 podcasts iniciales", () => {
+    render(<Principal />)
+    expect(screen.getByText("100")).toBeInTheDocument()
+  })
+  test("Debe mostrar input de filtro", () => {
+    render(<Principal />)
+    expect(screen.getByPlaceholderText("Filter podcasts...")).toBeInTheDocument()
+  })
 })
